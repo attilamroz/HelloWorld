bad5074653ac6ef24d99a174d5097766014e6606
attila.mroz <attila.mroz@AMS-GYM-EDV1-15.gym.ams.local>
2019-02-11 16:07:34 +0100

    Made Bubblesort.java

---
+++ HalloWelt/src/sort/BubbleSort.java
@@ -0,0 +1,38 @@
+package sort;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		int[] zahlen = { 3, 6, 1, 9, 2, 5 };
+
+		// zufall
+		for (int i = 0; i < zahlen.length; i++) {
+			long b = Math.round(Math.random() * 10);
+			int v = (int) b;
+			zahlen[i] = v;
+		}
+
+		// print zahlen
+		for (int i = 0; i < zahlen.length; i++) {
+			System.out.println(zahlen[i]);
+		}
+		System.out.println();
+
+		// Bubblesort
+		for (int n = zahlen.length; n > 1; n--) {
+			for (int i = 0; i < n - 1; i++) {
+				if (zahlen[i] > zahlen[i + 1]) {
+					int x = zahlen[i];
+					zahlen[i] = zahlen[i + 1];
+					zahlen[i + 1] = x;
+				}
+			}
+		}
+
+		// print zahlen
+		for (int i = 0; i < zahlen.length; i++) {
+			System.out.println(zahlen[i]);
+		}
+	}
+
+}
