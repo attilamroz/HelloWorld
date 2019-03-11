a17edbc481964887b7e797934453413ac5d842ef
attila.mroz <attila.mroz@AMS-GYM-EDV1-15.gym.ams.local>
2019-03-11 15:59:45 +0100

    QuickSort2 - Quicksort Erweiterung

---
+++ HalloWelt/src/sort/QuickSort2.java
@@ -0,0 +1,59 @@
+package sort;
+
+public class QuickSort2 {
+
+	public static void main(String[] args) {
+		
+		int[] zahlen = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+		
+		for (int i = 0; i < zahlen.length; i++) {
+			long b = Math.round(Math.random() * 50);
+			int v = (int) b;
+			zahlen[i] = v;
+		}
+		
+		for (int i = 0; i < zahlen.length; i++) {
+			System.out.print(zahlen[i] + " ");
+		}
+		System.out.println();
+		quicksort(0, zahlen.length - 1, zahlen);
+		for (int i = 0; i < zahlen.length; i++) {
+			System.out.print(zahlen[i] + " ");
+		}
+	}
+
+	public static void quicksort(int links, int rechts, int[] zahlen) {
+		if (links < rechts) {
+			int teiler = teile(links, rechts, zahlen);
+			quicksort(links, teiler - 1, zahlen);
+			quicksort(teiler + 1, rechts, zahlen);
+
+		}
+	}
+
+	public static int teile(int links, int rechts, int[] zahlen) {
+		int i = links;
+		int j = rechts - 1;
+		int pivot = zahlen[rechts];
+		do {
+			while (i < rechts - 1 && zahlen[i] < pivot) {
+				i = i + 1;
+			}
+			while (j > links && zahlen[j] >= pivot) {
+				j = j - 1;
+			}
+			if (i < j) {
+				int tauschvar = zahlen[i];
+				zahlen[i] = zahlen[j];
+				zahlen[j] = tauschvar;
+			}
+		} while (i < j);
+		if (zahlen[i] > pivot) {
+			int tauschvar = zahlen[i];
+			zahlen[i] = pivot;
+			zahlen[rechts] = tauschvar;
+		}
+		return i;
+	}
+
+}
