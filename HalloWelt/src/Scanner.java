13d78cbb32cad114031b1e6beabd4cc5439a7108
attila.mroz <attila.mroz@AMS-GYM-EDV1-15.gym.ams.local>
2019-03-11 16:11:56 +0100

    Scanner erweitert

--- HalloWelt/src/Scanner.java
+++ HalloWelt/src/Scanner.java
@@ -4,6 +4,7 @@ public class Scanner {
 	static java.util.Scanner scanner = new java.util.Scanner(System.in);
 	static String input = "";
 	static String zustand = "";
+	static String schlaf = "";
 	public static void main(String[] args) {
 		chapter1v1();
 	}
@@ -23,13 +24,45 @@ public class Scanner {
 		if(input.equals("Gut")) {
 			System.out.println(" Das freut mich.");
 			zustand = input;
-			//chapter1v3();
+			chapter1v3();
 		} else if(input.equals("Schlecht")) {
 			System.out.println(" Du armer.");
 			zustand = input;
-			//chapter1v3();
+			chapter1v3();
 		} else {
 			chapter1v2();
+		}
+	}
+	
+	public static void chapter1v3() {
+		System.out.println(" Und? Ausgeschlafen?");
+		input = scanner.nextLine();
+		if(input.equals("Ja")) {
+			System.out.println(" Das freut mich.");
+			schlaf = input;
+			chapter1v4();
+		} else if(input.equals("Nein")) {
+			System.out.println(" Du armer.");
+			schlaf = input;
+			chapter1v4();
+		} else {
+			chapter1v3();
+		}
+	}
+	
+	public static void chapter1v4() {
+		System.out.println(" Und? Ausgeschlafen?");
+		input = scanner.nextLine();
+		if(input.equals("Ja")) {
+			System.out.println(" Das freut mich.");
+			schlaf = input;
+			chapter1v3();
+		} else if(input.equals("Nein")) {
+			System.out.println(" Du armer.");
+			schlaf = input;
+			chapter1v3();
+		} else {
+			chapter1v3();
 		}
 	}
 
